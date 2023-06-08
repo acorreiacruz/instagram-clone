4fcde18a052ea01742507375092f3298053c85ca
acorreiacruz <antoniocruzjunior@outlook.com>
2023-06-08 18:54:51 -0300

    cors config.

--- back/core/settings.py
+++ back/core/settings.py
@@ -124,4 +124,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://localhost",
+]
